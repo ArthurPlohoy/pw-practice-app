34cfa3edaecf7da08b9687c9c54b58ccb5265d7f
Artur Plokhyi <arturplokhyi@gmail.com>
2026-04-22 23:37:09 +0300

    First test suite

---
+++ tests/firstTest.spec.ts
@@ -0,0 +1,13 @@
+import {test} from '@playwright/test';
+
+test.describe('First test suite', () => {
+    test.beforeEach(async ({ page }) => {
+        await page.goto('http://localhost:4200/');
+        await page.getByText('Forms').click();
+    });
+
+    test('my first test', async ({ page }) => {
+        await page.getByText('Form Layouts').click();
+    });
+
+});
