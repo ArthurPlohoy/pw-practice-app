1ef51c784836524bb2eb6bf1be98ad5291ccce44
Artur Plokhyi <arturplokhyi@gmail.com>
2026-04-23 23:08:57 +0300

    Locators

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -11,4 +11,24 @@ test.describe('First test suite', () => {
         await page.getByText('Basic Form').click();
     });
 
+    test('Local syntax rules', async ({ page }) => {
+        // by tag name 
+        page.locator('input')
+
+        // by class name
+        page.locator('.shape-rectangle')
+
+        // by id
+        page.locator('#inputEmail1')
+
+        // by attribute
+        page.locator('[placeholder="Email"]')
+
+        // combine different selectors
+        page.locator('input.shape-rectangle#inputEmail1[placeholder="Email"]')
+
+        // by xpath (Not recommended)
+        page.locator('//input[@id="inputEmail1"]')
+    });
+
 });
