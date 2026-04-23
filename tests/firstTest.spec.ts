8a44299d0dd0c02df0a07af08eb5880006838aff
Artur Plokhyi <arturplokhyi@gmail.com>
2026-04-24 00:07:04 +0300

    User facing locators

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -4,34 +4,52 @@ test.describe('First test suite', () => {
     test.beforeEach(async ({ page }) => {
         await page.goto('http://localhost:4200/');
         await page.getByText('Forms').click();
-    });
-
-    test('my first test', async ({ page }) => {
         await page.getByText('Form Layouts').click();
-        await page.getByText('Basic Form').click();
     });
 
-    test('Local syntax rules', async ({ page }) => {
-        // by tag name 
-        page.locator('input')
+    // test('my first test', async ({ page }) => {
+    //     await page.getByText('Form Layouts').click();
+    //     await page.getByText('Basic Form').click();
+    // });
 
-        // by class name
-        page.locator('.shape-rectangle')
+    test('User facing locators', async ({ page }) => {
+       await page.getByRole('textbox', { name: 'Email' }).first().click();
 
-        // by id
-        page.locator('#inputEmail1')
+       await page.getByRole('button', { name: 'Sign in' }).first().click();
 
-        // by attribute
-        page.locator('[placeholder="Email"]')
+       await page.getByLabel('Email').first().click();
 
-        // combine different selectors
-        page.locator('input.shape-rectangle#inputEmail1[placeholder="Email"]')
+       await page.getByPlaceholder('Jane Doe').first().click();
 
-        // by xpath (Not recommended)
-        page.locator('//input[@id="inputEmail1"]')
+       await page.getByText('Using the Grid').first().click();
 
-        // by text content
-        page.locator('text=Email')
+       await page.getByTestId('submit-button').first().click();
+
     });
+
+    // test('Local syntax rules', async ({ page }) => {
+    //     // by tag name 
+    //     page.locator('input')
+
+    //     // by class name
+    //     page.locator('.shape-rectangle')
+
+    //     // by id
+    //     page.locator('#inputEmail1')
+
+    //     // by attribute
+    //     page.locator('[placeholder="Email"]')
+
+    //     // combine different selectors
+    //     page.locator('input.shape-rectangle#inputEmail1[placeholder="Email"]')
+
+    //     // by xpath (Not recommended)
+    //     page.locator('//input[@id="inputEmail1"]')
+
+    //     // by text content
+    //     page.locator('text=Email')
+    // });
+
+    
 
 });
